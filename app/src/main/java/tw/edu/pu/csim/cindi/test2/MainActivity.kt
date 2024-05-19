67098ba0a61775a5dd5a0895c2e8cc26ba9dd770
cindili1215 <cindy1050286@gmail.com>
2024-05-19 20:13:55 +0800

    v0_李欣諦

---
+++ app/src/main/java/tw/edu/pu/csim/cindi/test2/MainActivity.kt
@@ -0,0 +1,61 @@
+package tw.edu.pu.csim.cindi.test2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.tooling.preview.Preview
+import tw.edu.pu.csim.cindi.test2.ui.theme.Test2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Test2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    //Greeting("")
+                    PointerEvents()
+                }
+            }
+        }
+    }
+}
+@Composable
+fun PointerEvents() {
+    var msg by remember { mutableStateOf("") }
+    Column {
+        Text(msg)
+        Box(
+            Modifier
+                .fillMaxSize()
+                .background(Color.Yellow)
+                .pointerInput(Unit) {
+                    awaitPointerEventScope {
+                        while (true) {
+                            val event = awaitPointerEvent()
+                            msg = "${event.type}, ${event.changes.first().position}"
+                        }
+                    }
+                }
+        )
+    }
+}
+
