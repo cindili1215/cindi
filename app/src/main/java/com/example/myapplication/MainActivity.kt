a26459923fb6108a82a572b6d23ca01d497c5a3c
cindili1215 <cindy1050286@gmail.com>
2024-05-19 21:28:55 +0800

    v1_李欣諦

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,15 +1,36 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,25 +40,58 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    FirstScreen()
+                    Main()
                 }
             }
         }
     }
 }
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun Main() {
+    val navController = rememberNavController()
+    val context = LocalContext.current
+    var showMenu by remember { mutableStateOf(false) }
+
+    Column {
+        TopAppBar(
+            title = { Text(text = "頁面轉換實例") },
+            navigationIcon = {
+                IconButton(onClick = {
+                    Toast.makeText(context, "您點選了導覽圖示", Toast.LENGTH_SHORT).show()
+                }) {
+                    Icon(Icons.Default.Menu, contentDescription = "Navigation icon")
+                }
+            }
+        )
+
+        NavHost(navController = navController, startDestination = "JumpFirst") {
+
+        }
+
+    }
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
+fun FirstScreen(){
+    val context = LocalContext.current  //取得App的運行環境
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.White),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start
+    )
+    {
+        Button(onClick = {
+            var it = Intent(context, SecondActivity::class.java)
+            context.startActivity(it)
+
+        })
+        {
+            Text(text = "跳轉到SecondActivity")
+        }
     }
 }
